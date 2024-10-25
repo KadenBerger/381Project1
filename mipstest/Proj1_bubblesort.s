0078dad8060f01080c1eacbbdecae252e13323b1
jjmetzen <jjmetzen@linuxvdi-02.ece.iastate.edu>
2024-10-25 12:54:39 -0500

    Updated

--- mipstest/Proj1_bubblesort.s
+++ mipstest/Proj1_bubblesort.s
@@ -2,57 +2,57 @@
 
 .data
 arr:
-        .word   5 #0 
-        .word   4 #1
-        .word   3 #2
-        .word   2 #3
-        .word	1 #4
+        .word   5  
+        .word   4 
+        .word   3 
+        .word   2 
+        .word	1 
 .text
 main:
-  li $sp, 0x10011000
-  sub	$sp, $sp, 32
-  addi	$a0, $sp, 8
-  addi	$a1, $zero, 20
-  sw	$a1, 4($sp)
-  addi	$t1, $zero, 5
-  sw	$t1, 0($a0)
-  addi	$t1, $zero, 4
-  sw	$t1, 4($a0)
-  addi	$t1, $zero, 3
-  sw	$t1, 8($a0)
+  li $sp, 0x10011000 	#Int stack pointer
+  sub	$sp, $sp, 32 	#make space on stack 
+  addi	$a0, $sp, 8 	#a0 is the array stack
+  addi	$a1, $zero, 20  #array size in bytes
+  sw	$a1, 4($sp) 	#stores array
+  addi	$t1, $zero, 5	#t1 loads 5
+  sw	$t1, 0($a0)	#a0(0) stores 5
+  addi	$t1, $zero, 4 	
+  sw	$t1, 4($a0)	#a0(1) stores 4
+  addi	$t1, $zero, 3	
+  sw	$t1, 8($a0)	#a0(2) stores 3	
   addi	$t1, $zero, 2
-  sw	$t1, 12($a0)
+  sw	$t1, 12($a0)	#a0(3) stores 2
   addi	$t1, $zero, 1
-  sw	$t1, 16($a0)
+  sw	$t1, 16($a0)	#a0(4) stores 1
   addi	$t1, $zero, 0
 bubble:
-  addi  $t8, $sp, 8     #pointer: pointer to a[]
-  addi  $t7, $zero, 0   #swapped
-  addi  $t6, $zero, 4   #int: i
+  addi  $t8, $sp, 8     #t8 is the start pointer
+  addi  $t7, $zero, 0   #t7 swapped
+  addi  $t6, $zero, 4   #t6 the index counter
 
 whileloop:
-  slt   $t9, $t6, $a1
-  bne   $t9, 1, swapped
-  lw	$t0, 0($t8)          #load a = a[i]
-  lw	$t1, 4($t8)          #load b = a[i+1]
-  slt	$t2, $t1, $t0        #if a[i+1] < a[i]
-  bne	$t2, 1, loop      #if a[i+1] > a[i], go to else
-  sw	$t1, 0($t8)          #a[i]   = a[i+1]
-  sw	$t0, 4($t8)          #a[i+1] = a[i]
-  addi	$t7, $zero, 1        #set swapped to true
+  slt   $t9, $t6, $a1	     #checks t6 less than array size
+  bne   $t9, 1, swapped	     #if flase than jump
+  lw	$t0, 0($t8)          #t0 loads a(i)
+  lw	$t1, 4($t8)          #t1 loads a(i+1)
+  slt	$t2, $t1, $t0        #checks a(i+1) less than a(i)
+  bne	$t2, 1, loop         #loop if not
+  sw	$t1, 0($t8)          #swap a(i) and a(i+1)
+  sw	$t0, 4($t8)          #swap a(i+1) and a(i)
+  addi	$t7, $zero, 1        #set swapped to 1
   j	loop                 #go to loop
 
 loop:
-  addi    $t6, $t6, 4           #i++
-  addi    $t8, $t8, 4           #a[]++
-  j       whileloop             #go back to loop
+  addi    $t6, $t6, 4           # increments i
+  addi    $t8, $t8, 4           # increments array
+  j       whileloop             #back to loop
 swapped:
-  beq	$t7, 1, bubble
+  beq	$t7, 1, bubble		#swap repeat bubble sort
 
 whiledone:
-  lw	$t0, 0($a0)
-  lw	$t1, 4($a0)
-  lw	$t2, 8($a0)
-  lw	$t3, 12($a0)
-  lw	$t4, 16($a0)
-  #halt
+  lw	$t0, 0($a0) 		#load a(0)
+  lw	$t1, 4($a0)		#load a(1)
+  lw	$t2, 8($a0)		#load a(2)
+  lw	$t3, 12($a0)		#load a(3)
+  lw	$t4, 16($a0)		#load a(4)
+  
--- mipstest/Proj1_cf_test.s
+++ mipstest/Proj1_cf_test.s
@@ -1,49 +1,50 @@
 #cfTest
+
 .data
 
 .text
 main:
-	li $sp, 0x10011000
-	addi	$a0, $zero, 0x0
-	addi	$a1, $zero, 0x1
-	addi	$a2, $zero, 0x2
-	jal	stackframetest
-	addi	$s0, $v0, 0
-	j	finish
+	li $sp, 0x10011000 	 #Int stack pointer 
+	addi	$a0, $zero, 0x0  #a0 = 0
+	addi	$a1, $zero, 0x1	 #a1 = 1
+	addi	$a2, $zero, 0x2  #a2 = 2
+	jal	stackframetest	 #call stack
+	addi	$s0, $v0, 0	 #store result in s0
+	j	finish		 #done
 
 stackframetest:
-	addi	$sp, $sp, -16
-	slti	$t4, $a2, 0xF00
-	beq	$t4, 0, testdone
-	sw	$ra, 0($sp)
-	sw	$a0, 4($sp)
-	sw	$a1, 8($sp)
-	sw	$a2, 12($sp)
-	addi	$v0, $v0, 1
-	j	stacklooptest
+	addi	$sp, $sp, -16    #allocate 16 bytes
+	slti	$t4, $a2, 0xF00	 #check if a2 less than imm
+	beq	$t4, 0, testdone #not than skip testdone
+	sw	$ra, 0($sp)	 #save ra
+	sw	$a0, 4($sp)	 #save a0
+	sw	$a1, 8($sp)	 #save a1
+	sw	$a2, 12($sp)	 #save a2
+	addi	$v0, $v0, 1	 #increment v0 by 1
+	j	stacklooptest	 #jump to loop
 	
 stacklooptest:
-	lw	$t0, 4($sp)
-	lw	$t1, 8($sp)
-	lw	$t2, 12($sp)
-	add	$t1, $t1, $t0
-	add	$t2, $t2, $t1
-	addi	$t0, $t0, 1
-	addi	$a2, $t2, 0
-	addi	$a1, $t1, 0
-	addi	$a0, $t0, 0
-	j	stackframetest
+	lw	$t0, 4($sp)	#t0 loads a0
+	lw	$t1, 8($sp)	#t1 loads a1
+	lw	$t2, 12($sp)	#t2 loads a2
+	add	$t1, $t1, $t0	#t2 = t1+t0
+	add	$t2, $t2, $t1	#t2 = t2 +t1
+	addi	$t0, $t0, 1	#increment t0 by 1
+	addi	$a2, $t2, 0	#update a2
+	addi	$a1, $t1, 0	#update a1
+	addi	$a0, $t0, 0	#update a0
+	j	stackframetest	#jump to start
 testdone:
-	addi	$sp, $sp, 16
-	lw	$t0, 4($sp)
-	lw	$t1, 8($sp)
-	lw	$t2, 12($sp)
-	add	$t1, $t1, $t0
-	add	$t2, $t2, $t1
-	add	$v1, $v1, $t2
-	bne	$t0, 1, testdone
-	lw	$ra, 0($sp)
-	jr	$ra
+	addi	$sp, $sp, 16	 #deallocate 16 bytes
+	lw	$t0, 4($sp)	 #t0 loads a0
+	lw	$t1, 8($sp)	 #t1 loads a1
+	lw	$t2, 12($sp)	 #t2 loads a2
+	add	$t1, $t1, $t0	 #t1 = t1+t0
+	add	$t2, $t2, $t1	 #t2 = t2+t1
+	add	$v1, $v1, $t2	 #v1 = v1+t2
+	bne	$t0, 1, testdone #loop to testdone if t0!=1
+	lw	$ra, 0($sp)	 #restore ra
+	jr	$ra		 #return to caller
 	
 finish:
-	#halt
+	
